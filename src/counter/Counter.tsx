9147a691e74b183446b76f7d734460352344777a
Kori Ryter <kryter@gmail.com>
2022-01-09 22:57:11 -0600

    Add form elements to counter page

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -5,9 +5,20 @@ function Counter() {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <button className="counter-button" type="button" onClick={() => setCount((count) => count + 1)}>
-        count is: {count}
-    </button>
+    <div className="counter-form-container">
+      <input className="title-text-box form-item" type="text" placeholder="Title goes here"/>
+
+      <textarea className="description-text-area form-item" />
+
+      <div className="checkbox-container form-item">
+        <input className="confirm-checkbox" type="checkbox" />
+        <div>I agree</div>
+      </div>
+
+      <button className="counter-button form-item" type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+      </button>
+    </div>
   );
 }
 
